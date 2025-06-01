07747f4475869fcdc37cd441c6b8713b6b5cf94a
jeromeku <jerome.ku@gmail.com>
2025-06-01 10:27:37 -0700

    add trace utils

---
+++ deep_gemm/trace.py
@@ -0,0 +1,66 @@
+from viztracer import VizTracer
+from viztracer import get_tracer as get_global_tracer
+
+import deep_gemm
+
+ROOT_DIR = deep_gemm.__path__[0]
+
+def _get_trace_config(
+    include_files=None,
+    ignore_frozen=True,
+    ignore_c_function=True,
+    log_func_args=True,
+    log_torch=False,
+    log_func_retval=True,
+    dump_raw=False,
+    output_file=None,
+    **kwargs,
+):
+    tracer_kwargs = dict(
+        include_files=include_files,
+        ignore_frozen=ignore_frozen,
+        ignore_c_function=ignore_c_function,
+        log_func_args=log_func_args,
+        log_func_retval=log_func_retval,
+        log_torch=log_torch,
+        dump_raw=dump_raw,
+        output_file=output_file,
+        **kwargs,
+    )
+    return tracer_kwargs
+
+
+def create_tracer(
+    include_files=None,
+    ignore_frozen=True,
+    ignore_c_function=True,
+    log_func_args=True,
+    log_torch=False,
+    log_func_retval=True,
+    dump_raw=False,
+    **kwargs,
+):
+    if include_files is None:
+        include_files = [ROOT_DIR]
+        print(f"Setting include_files to {include_files}")
+        
+    tracer = VizTracer(
+        include_files=include_files,
+        ignore_frozen=ignore_frozen,
+        ignore_c_function=ignore_c_function,
+        log_func_args=log_func_args,
+        log_func_retval=log_func_retval,
+        log_torch=log_torch,
+        dump_raw=dump_raw,
+        **kwargs
+    )
+    return tracer
+
+def configure_tracer(tracer: VizTracer = None, **kwargs):
+    if tracer is None:
+        tracer = get_global_tracer()
+    
+    for k,v in kwargs.items():
+        setattr(tracer, k, v)
+
+    return tracer
