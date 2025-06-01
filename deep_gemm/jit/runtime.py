1148492a08aa5842012e498e06120b01c6fa2f88
jeromeku <jerome.ku@gmail.com>
2025-06-01 13:59:31 -0700

    add links to CBD

--- deep_gemm/jit/runtime.py
+++ deep_gemm/jit/runtime.py
@@ -1,10 +1,10 @@
 import os
 import subprocess
 import time
-import torch
-import cuda.bindings.driver as cbd
-
 from typing import Any, Dict, Optional, Type
+
+import cuda.bindings.driver as cbd
+import torch
 from torch.utils.cpp_extension import CUDA_HOME
 
 
@@ -22,7 +22,7 @@ class Runtime:
             return False
 
         # Contains all necessary files
-        files = ['kernel.cubin']
+        files = ["kernel.cubin"]
         return all(os.path.exists(os.path.join(path, file)) for file in files)
 
     @staticmethod
@@ -34,34 +34,74 @@ class Runtime:
         raise NotImplemented
 
     def __call__(self, **kwargs) -> cbd.CUresult:
+        """
+        Params for `cuLibraryLoadFromFile`:
+        fileName (bytes) - File to load from
+        jitOptions (List[CUjit_option]) - Options for JIT
+        jitOptionsValues (List[Any]) - Option values for JIT
+        numJitOptions (unsigned int) - Number of options
+        libraryOptions (List[CUlibraryOption]) - Options for loading
+        libraryOptionValues (List[Any]) - Option values for loading
+        numLibraryOptions (unsigned int) - Number of options for loading
+        """
         # Load CUBIN
         if self.kernel is None:
             start_time = time.time_ns()
 
             # Load CUBIN
-            path = bytes(os.path.join(self.path, 'kernel.cubin'), 'utf-8')
-            result, self.lib = cbd.cuLibraryLoadFromFile(path, [], [], 0, [], [], 0)
-            assert result == cbd.CUresult.CUDA_SUCCESS, f'Failed to load library: {result}'
+            path = bytes(os.path.join(self.path, "kernel.cubin"), "utf-8")
+            # https://nvidia.github.io/cuda-python/cuda-bindings/latest/module/driver.html#cuda.bindings.driver.cuLibraryLoadFromFile
+
+            result, self.lib = cbd.cuLibraryLoadFromFile(
+                path,
+                [], # jitOptions
+                [], # jitOptionsValues
+                0, # numJitOptions
+                [],  # libraryOptions
+                [],  # libraryOptionValues
+                0,  # numLibraryOptions
+            )
+            assert result == cbd.CUresult.CUDA_SUCCESS, (
+                f"Failed to load library: {result}"
+            )
 
             # Extract the kernel name
             # TODO: use `cuda-bindings` API to do this (requires at least 12.8)
-            command = [f'{CUDA_HOME}/bin/cuobjdump', '-symbols', path]
-            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            command = [f"{CUDA_HOME}/bin/cuobjdump", "-symbols", path]
+            result = subprocess.run(
+                command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+            )
             assert result.returncode == 0
-            illegal_names = ['vprintf', '__instantiate_kernel', '__internal', '__assertfail']
+            illegal_names = [
+                "vprintf",
+                "__instantiate_kernel",
+                "__internal",
+                "__assertfail",
+            ]
             check_illegal = lambda line: any([name in line for name in illegal_names])
-            kernel_names = [line.split()[-1] for line in result.stdout.splitlines()
-                            if line.startswith('STT_FUNC') and not check_illegal(line)]
-            assert len(kernel_names) == 1, f'Too many kernels in the library: {kernel_names}'
-
+            kernel_names = [
+                line.split()[-1]
+                for line in result.stdout.splitlines()
+                if line.startswith("STT_FUNC") and not check_illegal(line)
+            ]
+            assert len(kernel_names) == 1, (
+                f"Too many kernels in the library: {kernel_names}"
+            )
+            breakpoint()
+            # https://nvidia.github.io/cuda-python/cuda-bindings/latest/module/driver.html#cuda.bindings.driver.cuLibraryEnumerateKernels
+            # https://nvidia.github.io/cuda-python/cuda-bindings/latest/module/driver.html#cuda.bindings.driver.cuLibraryGetKernelCount
             # Load kernel from the library
-            result, self.kernel = cbd.cuLibraryGetKernel(self.lib, bytes(kernel_names[0], encoding='utf-8'))
-            assert result == cbd.CUresult.CUDA_SUCCESS, f'Failed to load kernel: {result}'
+            result, self.kernel = cbd.cuLibraryGetKernel(
+                self.lib, bytes(kernel_names[0], encoding="utf-8")
+            )
+            assert result == cbd.CUresult.CUDA_SUCCESS, (
+                f"Failed to load kernel: {result}"
+            )
 
             end_time = time.time_ns()
             elapsed_time = (end_time - start_time) / 1e6
-            if int(os.getenv('DG_JIT_DEBUG', 0)):
-                print(f'Loading JIT runtime {self.path} took {elapsed_time:.2f} ms.')
+            if int(os.getenv("DG_JIT_DEBUG", 0)):
+                print(f"Loading JIT runtime {self.path} took {elapsed_time:.2f} ms.")
 
         # noinspection PyArgumentList
         return self.launch(self.kernel, kwargs)
@@ -70,7 +110,7 @@ class Runtime:
         if self.lib is not None:
             res = cbd.cuLibraryUnload(self.lib)[0]
             if res != cbd.CUresult.CUDA_SUCCESS:
-                raise Exception(f'Failed to unload library {self.path}: {res}')
+                raise Exception(f"Failed to unload library {self.path}: {res}")
 
 
 class RuntimeCache:
@@ -80,24 +120,42 @@ class RuntimeCache:
     def __setitem__(self, path: str, runtime: Runtime) -> None:
         self.cache[path] = runtime
 
-    def get(self, path: str, runtime_cls: Type[Runtime],
-            name: str = '', kwargs: Dict[str, Any] = None,
-            force_enable_cache: bool = False) -> Optional[Runtime]:
+    def get(
+        self,
+        path: str,
+        runtime_cls: Type[Runtime],
+        name: str = "",
+        kwargs: Dict[str, Any] = None,
+        force_enable_cache: bool = False,
+    ) -> Optional[Runtime]:
         # In Python runtime
         if path in self.cache:
             return self.cache[path]
 
         # Already compiled
-        use_cache = force_enable_cache or not int(os.getenv('DG_JIT_DISABLE_CACHE', 0))
+        use_cache = force_enable_cache or not int(os.getenv("DG_JIT_DISABLE_CACHE", 0))
         if use_cache and os.path.exists(path) and Runtime.is_path_valid(path):
             # Print heuristic for the first time
-            if name and (int(os.getenv('DG_JIT_DEBUG', 0)) or int(os.getenv('DG_PRINT_CONFIGS', 0))):
+            if name and (
+                int(os.getenv("DG_JIT_DEBUG", 0))
+                or int(os.getenv("DG_PRINT_CONFIGS", 0))
+            ):
                 simplified_kwargs = dict()
-                for key, value in kwargs.items() if kwargs is not None else dict().items():
-                    value = f'torch.Tensor<{value.dtype}>' if isinstance(value, torch.Tensor) else value
-                    value = f'cuda.bindings.driver.CUtensorMap' if isinstance(value, cbd.CUtensorMap) else value
+                for key, value in (
+                    kwargs.items() if kwargs is not None else dict().items()
+                ):
+                    value = (
+                        f"torch.Tensor<{value.dtype}>"
+                        if isinstance(value, torch.Tensor)
+                        else value
+                    )
+                    value = (
+                        f"cuda.bindings.driver.CUtensorMap"
+                        if isinstance(value, cbd.CUtensorMap)
+                        else value
+                    )
                     simplified_kwargs[key] = value
-                print(f'Put kernel {name} with {simplified_kwargs} into runtime cache')
+                print(f"Put kernel {name} with {simplified_kwargs} into runtime cache")
 
             runtime = runtime_cls(path)
             self.cache[path] = runtime
